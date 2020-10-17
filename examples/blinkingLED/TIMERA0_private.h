14ef76e017eb6e6d0a66d85ce7e29315abb6f4cb
Mohammed Ahmed <mohamedstarwars@gmail.com>
2020-10-17 09:19:41 +0200

    BlinkingLED example

---
+++ examples/blinkingLED/TIMERA0_private.h
@@ -0,0 +1,33 @@
+/*
+ * TIMER0A_private.h
+ *
+ *  Created on: Oct 14, 2020
+ *      Author: Mohammed
+ */
+
+#ifndef TIMERA0_PRIVATE_H_
+#define TIMERA0_PRIVATE_H_
+
+
+#define TA0CTL * ((u16*) 0x0340)
+#define TA0CCTL0 * ((u16*) 0x0342)
+#define TA0CCTL1 * ((u16*) 0x0344)
+#define TA0CCTL2 * ((u16*) 0x0346)
+#define TA0CCTL3 * ((u16*) 0x0348)
+#define TA0CCTL4 * ((u16*) 0x034A)
+#define TA0CCTL5 * ((u16*) 0x034C)
+#define TA0CCTL6 * ((u16*) 0x034E)
+#define TA0R * ((u16*) 0x0350)
+#define TA0CCR0 * ((u16*) 0x0352)
+#define TA0CCR1 * ((u16*) 0x0354)
+#define TA0CCR2 * ((u16*) 0x0356)
+#define TA0CCR3 * ((u16*) 0x0358)
+#define TA0CCR4 * ((u16*) 0x035A)
+#define TA0CCR5 * ((u16*) 0x035C)
+#define TA0CCR6 * ((u16*) 0x035E)
+#define TA0IV * ((u16*) 0x036E)
+#define TA0EX0 * ((u16*) 0x0360)
+
+
+
+#endif /* TIMERA0_PRIVATE_H_ */
