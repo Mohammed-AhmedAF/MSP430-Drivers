ce75f9ec5abb3ee8289e3c49efcf913b187627f9
Mohammed Ahmed <mohamedstarwars@gmail.com>
2020-11-21 21:16:20 +0200

    Init function taking input parameters, instead of void

--- lib/TIMERA0_program.c
+++ lib/TIMERA0_program.c
@@ -1,5 +1,3 @@
-/*
- */
 #include "Macros.h"
 #include "STD_TYPES.h"
 #include "msp430f5529.h"
@@ -13,25 +11,85 @@ void TIMERA0_vidPutISRFunction(void (*ptF) (void))
 }
 
 
-void TIMERA0_vidInit(void)
+void TIMERA0_vidInit(u8 u8TimerMode,u8 u8ClkSel, u8 u8ClkDivider, u16 u16CounterValue)
 {
-   /*Selecting the mode: Continuous*/
-   SET_BIT(TA0CTL,4);
-   CLEAR_BIT(TA0CTL,5);
+   /*Mode Control*/
+
+   switch(u8TimerMode)
+   {
+   case TIMERA0_MODE_STOP:
+       CLEAR_BIT(TA0CTL,4);
+       CLEAR_BIT(TA0CTL,5);
+       break;
+   case TIMERA0_MODE_UP:
+       SET_BIT(TA0CTL,4);
+       CLEAR_BIT(TA0CTL,5);
+       break;
+   case TIMERA0_MODE_CONTINUOUS:
+       CLEAR_BIT(TA0CTL,4);
+       SET_BIT(TA0CTL,5);
+       break;
+   case TIMERA0_MODE_UPDOWN:
+       SET_BIT(TA0CTL,4);
+       SET_BIT(TA0CTL,5);
+       break;
+   }
 
    /*Divider*/
-   SET_BIT(TA0CTL,6);
-   SET_BIT(TA0CTL,7);
+   switch(u8ClkDivider)
+   {
+   case TIMERA0_CLKDIV_1:
+       CLEAR_BIT(TA0CTL,6);
+       CLEAR_BIT(TA0CTL,7);
+       break;
+   case TIMERA0_CLKDIV_2:
+       SET_BIT(TA0CTL,6);
+       CLEAR_BIT(TA0CTL,7);
+       break;
+   case TIMERA0_CLKDIV_4:
+       CLEAR_BIT(TA0CTL,6);
+       SET_BIT(TA0CTL,7);
+       break;
+   case TIMERA0_CLKDIV_8:
+       SET_BIT(TA0CTL,6);
+       SET_BIT(TA0CTL,7);
+       break;
+   }
 
-    
-   /*Counting*/
-   TA0CCR0 = 10000;
 
+   if ((u8TimerMode == TIMERA0_MODE_CONTINUOUS) || (u8TimerMode == TIMERA0_MODE_UP) || (u8TimerMode == TIMERA0_MODE_UPDOWN))
+   {
+   /*Counter*/
+       TA0CCR0 = u16CounterValue;
+   }
    /*Selecting clock*/
-   CLEAR_BIT(TA0CTL,8);
-   SET_BIT(TA0CTL,9);
+   switch(u8ClkSel)
+   {
+   case TIMERA0_CLKSEL_TA0CLK:
+       CLEAR_BIT(TA0CTL,8);
+       CLEAR_BIT(TA0CTL,9);
+       break;
+   case TIMERA0_CLKSEL_AUX:
+       SET_BIT(TA0CTL,8);
+       CLEAR_BIT(TA0CTL,9);
+       break;
+   case TIMERA0_CLKSEL_SM:
+       CLEAR_BIT(TA0CTL,8);
+          SET_BIT(TA0CTL,9);
+          break;
+   case TIMERA0_CLKSEL_IN:
+       SET_BIT(TA0CTL,8);
+       SET_BIT(TA0CTL,9);
+       break;
+   }
+
+   TA0CCTL0 = CCIE; // CCR0 interrupt enabled
+}
 
 
+void TIMERA0_vidClearTimer(void)
+{
+    SET_BIT(TA0CTL,2);
 }
 
 
